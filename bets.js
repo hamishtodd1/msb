f8912a5a2882bc192062ef942141be37f624bb84
Hamish Todd <Hamish.Todd@imgtec.com>
2021-03-11 00:51:49 +0000

    changed bet movement method

--- bets.js
+++ bets.js
@@ -19,22 +19,16 @@ function bestowBets(suspect) {
         bet.owner = pm.BOARD_OWNERSHIP
     }
 
-    function getBottomMiddleTop(owner) {
-        if(owner === socket.id)
-            return 0
-        else if(owner === pm.BOARD_OWNERSHIP )
-            return 1
-        else
-            return 2
-    }
-
-    function getUnusedBetClosestToY(y) {
-        return getMax(bets, (bet, j) => {
+    function sendUnusedBetClosestToY(y) {
+        let closestBet = getMax(bets, (bet, j) => {
             if (betsAccountedFor[j] === true)
                 return -Infinity
             else
                 return -Math.abs(bet.position.y - y)
         })
+
+        closestBet.intendedPosition.y = y
+        betsAccountedFor[bets.indexOf(closestBet)] = true
     }
 
     const betsAccountedFor = Array(pm.betsPerSuspect)
@@ -54,9 +48,7 @@ function bestowBets(suspect) {
         for (let i = 0; i < numInHand; ++i) {
             let intendedY = suspect.handFrame.position.y + getSlotY(i)
 
-            let closestBet = getUnusedBetClosestToY(intendedY)
-            closestBet.intendedPosition.y = intendedY
-            betsAccountedFor[bets.indexOf(closestBet)] = true
+            sendUnusedBetClosestToY(intendedY)
         }
 
         //fill slots
@@ -67,9 +59,7 @@ function bestowBets(suspect) {
             if( i < pm.betsPerSuspect - numInBoard )
                 break
 
-            let closestBet = getUnusedBetClosestToY(slot.position.y)
-            closestBet.intendedPosition.y = slot.position.y
-            betsAccountedFor[bets.indexOf(closestBet)] = true
+            sendUnusedBetClosestToY(slot.position.y)
         }
 
         bets.forEach((bet, i) => {
@@ -83,38 +73,5 @@ function bestowBets(suspect) {
             bet.intendedPosition.x = suspect.frame.position.x
             bet.position.x = suspect.frame.position.x
         })
-
-
-        
-
-        // //sort by which thing they're in and how high up they are
-        // bets.sort((betA, betB) => { //gotta be negative if a < b
-        //     if (betA.owner === betB.owner)
-        //         return betA.position.y - betB.position.y
-
-        //     let aBmt = getBottomMiddleTop(betA.owner)
-        //     let bBmt = getBottomMiddleTop(betA.owner)
-        //     return aBmt - bBmt
-        // })
-
-        // bets.forEach((bet, i) => {
-        //     if (i < numInHand) //hand
-        //         bet.intendedPosition.y = suspect.handFrame.position.y + getSlotY(i)
-        //     else if (i < numInHand + numInBoard) { //board
-        //         i = i - numInHand
-        //         let slotIndex = pm.betsPerSuspect - (numInBoard - i)
-        //         bet.intendedPosition.y = suspect.boardFrame.position.y + getSlotY(slotIndex)
-        //     }
-        //     else //other player
-        //         bet.intendedPosition.y = EVERYONE_ELSE_VERTICAL_POSITION
-
-        //     if (frameCount === 1)
-        //         bet.goToIntendedPosition()
-        //     bet.intendedPosition.x = suspect.frame.position.x
-        //     bet.position.x = suspect.frame.position.x
-        // })
-
-        // log(bets[0].position.y)
-        // log(bets[bets.length-1].position.y)
     })
 }
